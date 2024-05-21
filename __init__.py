a7ad0ff6c89f300bcd3f63b7d9a93d93514eeb95
AntoonFML <131500917+AntoonFML@users.noreply.github.com>
2024-05-21 17:38:11 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-from ._core import __doc__, __version__, add, subtract, grafuj, dft, transformata, idft, odwrotna, plot, progowanie
+from ._core import __doc__, __version__, add, subtract, grafuj, dft, transformata, idft, odwrotna, plot, progowanie, real_to_complex
 
 __all__ = ["__doc__", "__version__", "add", "subtract"]
